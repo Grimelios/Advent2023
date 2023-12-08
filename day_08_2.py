960cceee1c84b02715c5e687e9d988bf14047f8b
Mark Hatfield <grimelios@gmail.com>
2023-12-08 11:58:47 -0600

    Day 8

--- day_08_2.py
+++ day_08_2.py
@@ -1,3 +1,32 @@
+from math import lcm
+
+def arrow_aligment(a, b, b_advantage):
+	period, phase = combine_phased_rotations(a, 0, b, -b_advantage % b)
+
+	return -phase % period
+
+def combine_phased_rotations(a_period, a_phase, b_period, b_phase):
+	gcd, s, t = extended_gcd(a_period, b_period)
+	phase_difference = a_phase - b_phase
+	pd_mult, pd_remainder = divmod(phase_difference, gcd)
+	combined_period = a_period // gcd * b_period
+	combined_phase = (a_phase - s * pd_mult * a_period) % combined_period
+
+	return combined_period, combined_phase
+
+def extended_gcd(a, b):	
+	old_r, r = a, b
+	old_s, s = 1, 0
+	old_t, t = 0, 1
+
+	while r:
+		quotient, remainder = divmod(old_r, r)
+		old_r, r = r, remainder
+		old_s, s = s, old_s - quotient * s
+		old_t, t = t, old_t - quotient * t
+
+	return old_r, old_s, old_t
+
 def find_loop(directions, nodes, node):
 	z_list = []
 	i = 0
@@ -32,6 +61,8 @@ def find_loop(directions, nodes, node):
 
 		path[pair] = steps
 
+	return combined_period, combined_phase
+
 with open('day_08.txt') as f:
 	lines = [line.rstrip() for line in f]
 
@@ -54,22 +85,14 @@ loops = []
 for n in starting:
 	loops.append(find_loop(directions, nodes, n))
 
-print('LOOPS %s' % loops)
-
-loop_a = loops.pop()
+loop_b = loops.pop()
 
 while len(loops) > 0:
-	loop_b = loops.pop() 
-	index_a = loop_a[0] + loop_a[2][0]
-	index_b = loop_b[0] + loop_b[2][0]
+	(offset_b, phase_b, z_b) = loop_b
+	(offset_a, phase_a, z_a) = loops.pop()
+	advantage = (offset_b + z_b[0]) - (offset_a + z_a[0])
+	meet = arrow_aligment(phase_a, phase_b, advantage)
+	z = meet + min(offset_a, offset_b)
+	loop_b = (z, lcm(phase_a, phase_b), [0])
 
-	print('A %s -> %s' % (index_a, loop_a[1]))
-	print('B %s -> %s' % (index_b, loop_b[1]))
-
-	while index_a != index_b:
-		index_a += loop_a[1]
-		index_b += loop_b[1]
-
-	print('MEET %s' % index_a)
-
-	quit()
+print(z)
